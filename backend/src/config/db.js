69c9b63ef8d4237b706b7b80e376fae0901b2ec6
k1212gh <145628649+k1212gh@users.noreply.github.com>
2025-04-01 10:07:48 +0900

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "44.204.8.221",  // AWS 서버 IP
-  user: "KevDev",
-  password: "!Kevin8578",
-  database: "todo_app",  // ✅ todo_app 데이터베이스 사용
+  host: "",  // AWS 서버 IP
+  user: "",
+  password: "",
+  database: "",  // ✅ todo_app 데이터베이스 사용
   waitForConnections: true,
   connectionLimit: 10,
 });
